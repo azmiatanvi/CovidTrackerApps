d8fd637a607be3026cca45ac08512c51397d791f
mabdulgani60 <muhammadabdulgani60@gmail.com>
2020-11-01 22:09:26 +0700

    tampilan statistics

--- app/src/main/java/com/example/covid_19/StatisticsActivity.java
+++ app/src/main/java/com/example/covid_19/StatisticsActivity.java
@@ -2,13 +2,30 @@ package com.example.covid_19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class StatisticsActivity extends AppCompatActivity {
+    ImageView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+
+        back = findViewById(R.id.btn_back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(StatisticsActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
     }
 }
